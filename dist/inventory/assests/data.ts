80bda261d1bc875a5f686b002b801d7a885dbfac
Balacheran <125751467+Balacheran@users.noreply.github.com>
2023-08-22 23:35:11 +0530

    Add files via upload

---
+++ dist/inventory/assests/data.ts
@@ -0,0 +1,31 @@
+export const COUNTRY = [
+    {
+        "name": "AAA",
+        "iso2": "AF",
+        "code": "93"
+    },
+    {
+        "name": "BBB",
+        "iso2": "AL",
+        "code": "355",
+        "status": "false"
+    },
+    {
+        "name": "CCC",
+        "iso2": "DZ",
+        "code": "213",
+        "status": "false"
+    },
+    {
+        "name": "DDD",
+        "iso2": "AS",
+        "code": "1 684",
+        "status": "false"
+    },
+    {
+        "name": "EEEE",
+        "iso2": "AD",
+        "code": "376",
+        "status": "false"
+    }
+]
---
+++ dist/inventory/assests/login-credentails.ts
@@ -0,0 +1,23 @@
+export const CREDENTIALS = 
+    {
+        "name": "admin",
+        "password": "123456",
+        "role": "admin"
+    }
+
+    export const USERCREDENTIALS = 
+    {
+        "name": "user001",
+        "password": "123456",
+        "role": "user"
+    }
+
+export const validUser = {
+        username: '00132126',
+        password: '123456'
+    };
+
+    export const blankUser = {
+        name: '',
+        password: ''
+    };
